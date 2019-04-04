35c64a1166597ea34bdff7e44665ed3539321766
Kellermeyer,Doug <dkellermeyer#paychex.com>
2019-04-04 11:21:11 -0400

    playing around with `this`

---
+++ thisAndObjectProperties/Chapter1/this.js
@@ -0,0 +1,39 @@
+//playing around with this and how it works
+
+const giveMeX = () => {
+    return this.x;
+};
+
+const newX = {
+    x: 2
+};
+
+const refToGiveMeX = giveMeX.call(newX); // 2
+
+console.log(refToGiveMeX);
+
+
+//example from YouDontKnowJS:
+
+function identify() {
+    return this.name.toUpperCase();
+}
+
+function speak() {
+    var greeting = "Hello, I'm " + identify.call( this );
+    console.log( greeting );
+}
+
+var me = {
+    name: "Doug"
+};
+
+var you = {
+    name: "Reader"
+};
+
+identify.call( me ); // DOUG
+identify.call( you ); // READER
+
+speak.call( me ); // Hello, I'm DOUG
+speak.call( you ); // Hello, I'm READER
