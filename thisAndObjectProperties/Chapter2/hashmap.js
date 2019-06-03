22cc1602cd6c25b5defec787006e7e23e7713d6c
Kellermeyer,Doug <dkellermeyer#paychex.com>
2019-06-03 15:52:04 -0400

    more hash

--- thisAndObjectProperties/Chapter2/hashmap.js
+++ thisAndObjectProperties/Chapter2/hashmap.js
@@ -1,15 +1,18 @@
-function hashMap(s){
-    obj = {}
+//parse the string and create a hashmap for each letter
 
-    for(i = 0; i < s.length; i++){
-        const letter = s[i]
-        if(obj[letter]){
-            obj[letter] += 1
+function hashMap(s){
+    obj = {} //empty object
+    
+    //loop throught the string
+    for(i = 0; i < s.length; i++){ 
+        const letter = s[i] 
+        if(obj[letter]){ //is the letter already in the obj?
+            obj[letter] += 1 //if so, add +1 to that letters key, ex. a:2
         }else {
-            obj[letter] = 1
+            obj[letter] = 1 //if the letter isn't there, make it there, ex. c:1
         }
     }
     return obj
 }
 
-console.log(hashMap("aabbccdddeeefff"))
+console.log(hashMap("aabbbc")) // => { a: 2, b: 3, c: 1 }
