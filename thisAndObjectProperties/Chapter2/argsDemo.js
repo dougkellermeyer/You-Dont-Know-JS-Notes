f0b59a6988cfe86821cb6d2a56a3dd1cc90cc1ba
Kellermeyer,Doug <dkellermeyer#paychex.com>
2019-06-04 10:31:34 -0400

    more args practice

--- thisAndObjectProperties/Chapter2/argsDemo.js
+++ thisAndObjectProperties/Chapter2/argsDemo.js
@@ -6,3 +6,12 @@ function argsDemo(a, b, ...args) {
 argsDemo(1,2,3,4,5,6,7);
 // `...arguments` => 1 2 3 4 5 6 7
 // `...args` ======> 3 4 5 6 7
+
+const numbers = [1, 2, 3];
+const add3 = (a, b, c) => console.log(a + b + c);
+add3(...numbers); // => add3(numbers[0], numbers[1], numbers[2], ...)
+
+function dial(...grandma) {
+	console.log(grandma);
+}
+dial(8, 6, 7, 5, 3, 0, 9);
